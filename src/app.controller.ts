71c12d09cc83c906fc6eecd8322d9605515c31df
kauan777 <kauan7622@gmail.com>
2022-12-15 09:21:47 -0300

    add comments

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+/* Atribuindo decorator @Controller a class AppController
+ para indicar que esta class é um Controller */
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  /* Como primeiro parâmetro, o método/decorator Get pode receber o caminho da rota
+  assim como o @Controller também. O que faira que a rota do get ficasse dentro da rota do controller */
 
   @Get()
   getHello(): string {
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,14 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [],
+
+  /* No controller não precisa passar as dependências como parâmetro, pois o nest trabalha com
+  a inserção de dependências, o que faz a instância da class 'service' ser passada como parãmetro.
+  Porém o service precisa ter o decorator @Injectable() */
+
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+// Para classes abstratas, muda a estrutura do 'providers'
