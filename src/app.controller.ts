3602df2a0739d0158357ab1f7c2ba775d7eca510
kauan777 <kauan7622@gmail.com>
2022-12-15 10:32:47 -0300

    feat: add methods list and create notification

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'crypto';
 
 /* Atribuindo decorator @Controller a class AppController
  para indicar que esta class é um Controller */
 
-@Controller()
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   /* Como primeiro parâmetro, o método/decorator Get pode receber o caminho da rota
   assim como o @Controller também. O que faira que a rota do get ficasse dentro da rota do controller */
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create() {
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content: 'Você tem uma nova solicitação de amizade',
+        category: 'social',
+        recipientId: randomUUID(),
+      },
+    });
   }
 }
