6b7f0453d09142e587dc4b6a00d3fbcdfba81665
kauan777 <kauan7622@gmail.com>
2022-12-19 18:12:41 -0300

    refactor: add imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './infra/app.controller';
-import { PrismaService } from './infra/prisma.service';
+import { DatabaseModule } from './infra/database/database.module';
+import { HttpModule } from './infra/http/http.module';
 
 @Module({
-  imports: [],
-
-  /* No controller não precisa passar as dependências como parâmetro, pois o nest trabalha com
-  a inserção de dependências, o que faz a instância da class 'service' ser passada como parãmetro.
-  Porém o service precisa ter o decorator @Injectable() */
-
-  controllers: [AppController],
-  providers: [PrismaService],
+  imports: [HttpModule, DatabaseModule],
 })
 export class AppModule {}
+
+/* No controller não precisa passar as dependências(services) como parâmetro, pois o nest trabalha com
+  a inserção de dependências, o que faz a instância da class 'service' ser passada como parãmetro.
+  Porém o service precisa ter o decorator @Injectable() e o service precisa ta em 'providers' */
 
 // Para classes abstratas(contrato), muda a estrutura do 'providers'
 
