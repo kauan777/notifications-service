5571524789fa6f3dd9afb3ec82d877b4ab02ca4c
kauan777 <kauan7622@gmail.com>
2022-12-19 18:03:37 -0300

    feat: creating useCase with contract

---
+++ src/app/use-cases/send-notification.ts
@@ -0,0 +1,41 @@
+import { Content } from '../entities/content';
+import { Injectable } from '@nestjs/common';
+import { Notification } from '../entities/notification';
+import { NotificationsRepository } from '../repositories/notifications-repository';
+
+interface SendNotificationRequest {
+  recipientId: string;
+  content: string;
+  category: string;
+}
+
+interface SendNotificationResponse {
+  notification: Notification;
+}
+
+@Injectable()
+export class SendNotification {
+  constructor(private notificationRepository: NotificationsRepository) {}
+
+  async execute(
+    request: SendNotificationRequest,
+  ): Promise<SendNotificationResponse> {
+    const { recipientId, content, category } = request;
+
+    const notification = new Notification({
+      recipientId,
+      content: new Content(content),
+      category,
+    });
+
+    await this.notificationRepository.create(notification);
+
+    return { notification };
+  }
+}
+
+// Unico método 'execute' seguindo os principios da responsabilidade única do solid
+
+// Receber objectos e sempre que possivel retornar objetos
+
+// 01:30:23
