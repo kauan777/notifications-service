b465dbd10ec71c18ea9728b05bf06801a4a6a48f
kauan777 <kauan7622@gmail.com>
2022-12-19 18:06:53 -0300

    feat: creating controller with all RN

---
+++ src/infra/http/controllers/notifications.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { SendNotification } from 'src/app/use-cases/send-notification';
+import { CreateNotificationBody } from '../dtos/create-notification-body';
+
+@Controller('notifications')
+export class NotificationsController {
+  constructor(private sendNotification: SendNotification) {}
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { recipientId, content, category } = body;
+
+    const { notification } = await this.sendNotification.execute({
+      category,
+      content,
+      recipientId,
+    });
+
+    return { notification };
+  }
+}
+
+/* Atribuindo decorator @Controller a class AppController
+ para indicar que esta class é um Controller */
+
+/* Como primeiro parâmetro, o método/decorator GET pode receber o caminho da rota
+  assim como o @Controller também. O que faira que a rota do get ficasse dentro da rota do controller */
