d047419f80d2e2eec5199d2293a5e5fc388d764a
kauan777 <kauan7622@gmail.com>
2022-12-22 15:15:33 -0300

    test: creating and adding logic with repositoryInMemory

---
+++ src/app/use-cases/read-notification.spec.ts
@@ -0,0 +1,34 @@
+import { makeNotification } from '@test/factories/notification-factory';
+import { InMemoryNotificationRepository } from '@test/repositories/in-memory-notifications-repository';
+import { NotificationNotFound } from './errors/notification-not-found';
+import { ReadNotification } from './read-notification';
+
+describe('Read notification', () => {
+  it('should be able to read a notification', async () => {
+    const notificationRepository = new InMemoryNotificationRepository();
+    const readNotification = new ReadNotification(notificationRepository);
+
+    const notification = makeNotification();
+
+    await notificationRepository.create(notification);
+
+    await readNotification.execute({
+      notificationid: notification.id,
+    });
+
+    expect(notificationRepository.notifications[0].readAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('should not be able to read a no exististing notification', async () => {
+    const notificationRepository = new InMemoryNotificationRepository();
+    const readNotification = new ReadNotification(notificationRepository);
+
+    expect(() => {
+      return readNotification.execute({
+        notificationid: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
