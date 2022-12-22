9aa7eb44c551d48a04d7dc4961199a142e6e95bc
kauan777 <kauan7622@gmail.com>
2022-12-22 15:01:08 -0300

    chore: add paths of the tsconfig.json

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,24 @@
-export default {
+import { Config } from 'jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
+// Para fazer importações de arquivos JSON, no tsconfig.json defina resolveJsonModule: true
+
+const config: Config = {
   moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'src',
   testRegex: '.*\\.spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
   collectCoverageFrom: ['**/*.(t|j)s'],
   coverageDirectory: '../coverage',
   testEnvironment: 'node',
 };
+
+export default config;
+
+// Por padrão o jest não entende os paths do tsconfig
+// Essa função pathsToModuleNameMapper converte os paths do tsconfig.json para o formato que o jest aceita
