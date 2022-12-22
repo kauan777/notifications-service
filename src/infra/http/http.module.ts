a0203c643b259d7c73cdeebd412aad0b1a2325e3
kauan777 <kauan7622@gmail.com>
2022-12-22 15:06:46 -0300

    refactor: add new providers

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { SendNotification } from '@app/use-cases/send-notification';
 import { DatabaseModule } from '../database/database.module';
 import { NotificationsController } from './controllers/notifications.controller';
+import { CancelNotification } from '@app/use-cases/cancel-notification';
+import { UnreadNotification } from '@app/use-cases/unread-notification';
+import { ReadNotification } from '@app/use-cases/read-notification';
+import { GetRecipientNotifications } from '@app/use-cases/get-recipient-notifications';
+import { CountRecipientNotifications } from '@app/use-cases/count-recipient-notification';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    UnreadNotification,
+    ReadNotification,
+    GetRecipientNotifications,
+    CountRecipientNotifications,
+  ],
 })
 export class HttpModule {}
