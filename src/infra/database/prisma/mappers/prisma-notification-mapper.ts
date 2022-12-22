dd961a49feac9f3de96717a250514f4b2cab05bf
kauan777 <kauan7622@gmail.com>
2022-12-22 15:09:13 -0300

    feat: creating mappers to convert class Notification and Prisma Entitie

---
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -0,0 +1,30 @@
+import { Notification as RawNotification } from '@prisma/client';
+import { Notification } from '@app/entities/notification';
+import { Content } from '@app/entities/content';
+
+export class PrismaNotificationMapper {
+  static toPrisma(notification: Notification) {
+    return {
+      id: notification.id,
+      content: notification.content.value,
+      recipientId: notification.recipientId,
+      category: notification.category,
+      readAt: notification.readAt,
+      createdAt: notification.createdAt,
+    };
+  }
+
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new Content(raw.content),
+        recipientId: raw.recipientId,
+        readAt: raw.readAt,
+        createdAt: raw.createdAt,
+        canceledAt: raw.canceledAt,
+      },
+      raw.id,
+    );
+  }
+}
