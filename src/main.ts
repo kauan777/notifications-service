2d6665d0e362253de5c593f4e40b3e27dbc53186
kauan777 <kauan7622@gmail.com>
2023-01-05 09:08:23 -0300

    feat: adding default configs kafka

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
+import { KafkaCosumerService } from '@infra/messaging/kafka/kafka-consumer.service';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions } from '@nestjs/microservices/interfaces';
 import { AppModule } from './app.module';
 
 declare const module: any;
@@ -8,6 +10,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const kafkaCosumerService = app.get(KafkaCosumerService);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    strategy: kafkaCosumerService,
+  });
+
+  await app.startAllMicroservices();
   await app.listen(3000);
 
   if (module.hot) {
